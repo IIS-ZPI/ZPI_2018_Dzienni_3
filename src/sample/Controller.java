c61d7e7c339e2c17c346de6323862a11bdf31783
unknown <Student@L403-12.carbon.cti.p.lodz.pl>
2018-05-09 15:43:55 +0200

    Added Layout

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,12 @@
 package sample;
 
+import javafx.fxml.FXML;
+
+import javax.swing.table.TableColumn;
+
 public class Controller {
+
+    @FXML
+    private TableColumn statesTableView;
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,21 +1,36 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    VBox root;
+
+    private ObservableList<String> states = FXCollections.observableArrayList();
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("layout.fxml"));
+        root = loader.load();
+
+        init();
+
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
+
+    public void init(){
+        states.add("Colorado");
+        states.add("Massecuses");
+    }
 
     public static void main(String[] args) {
         launch(args);
